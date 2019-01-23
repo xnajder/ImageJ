fae03e65bb22ef24a385b9a8af4b3c73fbef64df
xnajder <x.najder@gmail.com>
2019-01-23 16:35:59 +0100

    lets have a look in the macro

---
+++ RLE_Plugin.java
@@ -0,0 +1,9 @@
+// This plugin which analyses the cells tracks from Amira files.
+// (c) Karolina Najder
+
+
+public class My_Plugin implements PlugIn {
+    public void run(String arg) {
+        IJ.log(“Hello, World!”);
+    }
+}
